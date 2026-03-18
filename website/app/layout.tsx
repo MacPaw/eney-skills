162c1423450e58c5e35642a3ac5464fd8f99b9fa
Yurii Sokyran <sokyran@macpaw.com>
2026-03-18 17:54:25 +0200

    feat: add search to website/

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -1,4 +1,4 @@
-import { RootProvider } from "fumadocs-ui/provider/next";
+import { Provider } from "./provider";
 import type { ReactNode } from "react";
 import "./global.css";
 
@@ -9,7 +9,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         <link rel="icon" href="/eney-logo.png" />
       </head>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
